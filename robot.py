b5be57dd17c2f08a3038b25e797326228e7f7359
IamWangYunKai <615349118@163.com>
2022-04-10 19:05:12 +0800

    test success

--- robot.py
+++ robot.py
@@ -7,10 +7,14 @@ import cv2
 import numpy as np
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Image
 
+V_AMP = 3.0
+W_AMP = 2.0
+
 def execute_cmd(ros_cmd):
+    global cmd_pub
     cmd_pub.publish(ros_cmd)
 
 def parse_cmd(message):
@@ -18,9 +22,13 @@ def parse_cmd(message):
     cmd.ParseFromString(message)
 
     ros_cmd = Twist()
-    ros_cmd.linear.x = cmd.v
-    ros_cmd.angular.z = cmd.w
-    execute_cmd(cmd)
+    linear = Vector3()
+    angular = Vector3()
+    linear.x = cmd.v * V_AMP
+    angular.z = - cmd.w * W_AMP
+    ros_cmd.linear = linear
+    ros_cmd.angular = angular
+    execute_cmd(ros_cmd)
 
 class Client(Informer):
     def send_img(self, message):
@@ -29,25 +37,21 @@ class Client(Informer):
     def cmd_recv(self):
         self.recv('cmd', parse_cmd)
 
-ifm = Client(
-    config = 'config.yaml',
-    )
-
 def callback_img(ros_img):
-    img = np.ndarray(shape=(480, 640, 3), dtype=np.dtype("uint8"), buffer=ros_img.data)
+    img = np.ndarray(shape=(720, 1280, 3), dtype=np.dtype("uint8"), buffer=ros_img.data)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # img = cv2.resize(img, (320*4, 4*240))
+    img = cv2.resize(img, (320*2, 2*240))
     ret, jpeg = cv2.imencode('.jpg', img)
     data = jpeg.tobytes()
     ifm.send_img(data)
-    # print('send !')
-
 
 if __name__ == '__main__':
     rospy.init_node('5g-transfer', anonymous=True)
     cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=0)
     rospy.Subscriber('/camera/color/image_raw', Image, callback_img)
-
     rate = rospy.Rate(1000)
+
+    ifm = Client(config = 'config.yaml')
+
     while not rospy.is_shutdown():
         rate.sleep()
